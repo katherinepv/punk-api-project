89bb2caf7d0c77df6189b64ce063e3755b6bbf59
katherine <pv.katherine7@gmail.com>
2022-12-12 20:35:09 +0000

    created functions or arrays for all filters needed

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,103 @@ import { useEffect } from "react";
 // import SearchMenu from "./containers/SearchMenu/SearchMenu";
 
 const App = () => {
-  const [beers, setBeers] = useState([]);
-
-  const getBeers = async () => {
+  // all beers from page 1 (0-80)
+  const [beersPageOne, setBeersPageOne] = useState([]);
+  const getBeersPageOne = async () => {
     const response = await fetch(
       "https://api.punkapi.com/v2/beers?page=1&per_page=80"
     );
     const data = await response.json();
-    setBeers(data);
+    setBeersPageOne(data);
+  };
+
+  useEffect(() => {
+    getBeersPageOne();
+  }, []);
+
+  // all beers from page 2 (81-160)
+  const [beersPageTwo, setBeersPageTwo] = useState([]);
+  const getBeersPageTwo = async () => {
+    const response = await fetch(
+      "https://api.punkapi.com/v2/beers?page=2&per_page=80"
+    );
+    const data = await response.json();
+    setBeersPageTwo(data);
+  };
+
+  useEffect(() => {
+    getBeersPageTwo();
+  }, []);
+
+  // all beers from page 3 (161-240)
+  const [beersPageThree, setBeersPageThree] = useState([]);
+  const getBeersPageThree = async () => {
+    const response = await fetch(
+      "https://api.punkapi.com/v2/beers?page=3&per_page=80"
+    );
+    const data = await response.json();
+    setBeersPageThree(data);
+  };
+
+  useEffect(() => {
+    getBeersPageThree();
+  }, []);
+
+  // all beers from page 4 (241-320)
+  const [beersPageFour, setBeersPageFour] = useState([]);
+  const getBeersPageFour = async () => {
+    const response = await fetch(
+      "https://api.punkapi.com/v2/beers?page=4&per_page=80"
+    );
+    const data = await response.json();
+    setBeersPageFour(data);
+  };
+
+  useEffect(() => {
+    getBeersPageFour();
+  }, []);
+
+  // all beers from page 5 (320-325)
+  const [beersPageFive, setBeersPageFive] = useState([]);
+  const getBeersPageFive = async () => {
+    const response = await fetch(
+      "https://api.punkapi.com/v2/beers?page=5&per_page=80"
+    );
+    const data = await response.json();
+    setBeersPageFive(data);
+  };
+
+  useEffect(() => {
+    getBeersPageFive();
+  }, []);
+
+  const allBeers = beersPageOne.concat(
+    beersPageTwo,
+    beersPageThree,
+    beersPageFour,
+    beersPageFive
+  );
+  console.log(allBeers);
+
+  // all beers from classic range
+  const [classicBeers, setClassicBeers] = useState([]);
+  const getClassicBeers = async () => {
+    const response = await fetch(
+      "https://api.punkapi.com/v2/beers?brewed_before=01-2010&per_page=80"
+    );
+    const data = await response.json();
+    setClassicBeers(data);
     console.log(data);
   };
 
   useEffect(() => {
-    getBeers();
+    getClassicBeers();
   }, []);
 
   return (
     <div>
       <Nav />
-      {beers && <BeerContainer beersArr={beers} />}
+      {beersPageOne && <BeerContainer beersArr={allBeers} />}
     </div>
   );
 };
--- src/containers/BeerContainer/BeerContainer.jsx
+++ src/containers/BeerContainer/BeerContainer.jsx
@@ -6,23 +6,24 @@ const BeerContainer = ({ beersArr }) => {
     <Card key={id} name={name} image={image_url} tagline={tagline} />
   ));
 
-  // const highAbvCardsJSX = beersArr
-  //   .filter((beer) => beer.abv >= 6)
-  //   .map(({ id, name, image_url, tagline }) => (
-  //     <Card key={id} name={name} image={image_url} tagline={tagline} />
-  //   ));
+  const highAbvCardsJSX = beersArr
+    .filter((beer) => beer.abv >= 6)
+    .map(({ id, name, image_url, tagline }) => (
+      <Card key={id} name={name} image={image_url} tagline={tagline} />
+    ));
 
-  // const acidicCardsJSX = beersArr
-  //   .filter((beer) => beer.ph <= 4)
-  //   .map(({ id, name, image_url, tagline }) => (
-  //     <Card key={id} name={name} image={image_url} tagline={tagline} />
-  //   ));
+  const acidicCardsJSX = beersArr
+    .filter((beer) => beer.ph <= 4)
+    .map(({ id, name, image_url, tagline }) => (
+      <Card key={id} name={name} image={image_url} tagline={tagline} />
+    ));
+  console.log(acidicCardsJSX);
 
   return (
     <div>
-      {/* <div className="card-container">{highAbvCardsJSX}</div> */}
+      <div className="card-container">{acidicCardsJSX}</div>
       <h3>All beers</h3>
-      <div className="card-container">{allBeerCardsJSX}</div>
+      {/* <div className="card-container">{allBeerCardsJSX}</div> */}
     </div>
   );
 };
