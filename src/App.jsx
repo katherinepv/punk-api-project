fea973fc99c363588460a67ceea9e31b9bdda3c9
katherine <pv.katherine7@gmail.com>
2022-12-12 17:07:38 +0000

    checked beers from API have been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import "./App.scss";
-import beers from "./data/beers";
 import Nav from "./containers/Nav/Nav";
-
-import Main from "./containers/Main/Main";
-import SearchMenu from "./containers/SearchMenu/SearchMenu";
+import BeerContainer from "./containers/BeerContainer/BeerContainer";
+import { useState } from "react";
+import { useEffect } from "react";
+// import SearchMenu from "./containers/SearchMenu/SearchMenu";
 
 const App = () => {
+  const [beers, setBeers] = useState([]);
+
+  const getBeers = async () => {
+    const response = await fetch(
+      "https://api.punkapi.com/v2/beers?page=1per_page=80"
+    );
+    const data = await response.json();
+    setBeers(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getBeers();
+  }, []);
+
   return (
     <div>
       <Nav />
-      <Main title="All Beers" beersArr={beers} />
+      {beers && <BeerContainer beersArr={beers} />}
     </div>
   );
 };
---
+++ src/containers/BeerContainer/BeerContainer.jsx
@@ -0,0 +1,30 @@
+import "./BeerContainer.scss";
+import Card from "../../components/Card/Card";
+
+const BeerContainer = ({ beersArr }) => {
+  const allBeerCardsJSX = beersArr.map(({ id, name, image_url, tagline }) => (
+    <Card key={id} name={name} image={image_url} tagline={tagline} />
+  ));
+
+  const highAbvCardsJSX = beersArr
+    .filter((beer) => beer.abv >= 6)
+    .map(({ id, name, image_url, tagline }) => (
+      <Card key={id} name={name} image={image_url} tagline={tagline} />
+    ));
+
+  const acidicCardsJSX = beersArr
+    .filter((beer) => beer.ph <= 4)
+    .map(({ id, name, image_url, tagline }) => (
+      <Card key={id} name={name} image={image_url} tagline={tagline} />
+    ));
+
+  return (
+    <div>
+      <div className="card-container">{highAbvCardsJSX}</div>
+      <h3>All beers</h3>
+      <div className="card-container">{allBeerCardsJSX}</div>
+    </div>
+  );
+};
+
+export default BeerContainer;
